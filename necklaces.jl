a60a6138ee826287ba60f40d2842e225db74c703
Michael-Howes <michaelnelsonhowes@gmail.com>
2025-11-10 12:34:24 -0800

    add docstrings

--- necklaces.jl
+++ necklaces.jl
@@ -1,6 +1,11 @@
 using Random, Primes
 
-function sample_fixed_point(j::Int, n::Int, k=2::Int)
+"""
+    sample_fixed_point(j::Int, n::Int, k=2::Int)::Vector{Int}
+
+Given j in the cyclic group of order n, sample a k-ary vector fixed by j.
+"""
+function sample_fixed_point(j::Int, n::Int, k=2::Int)::Vector{Int}
     @assert 0 ≤ j "j must be greater than 0."
     @assert j < n "j must be strictly less than n."
 
@@ -12,6 +17,11 @@ function sample_fixed_point(j::Int, n::Int, k=2::Int)
     return x
 end
 
+"""
+    sample_fixed_point(x::Vector{Int})::Int
+
+Given a vector of length n, sample an element of the cyclic group fixing j.
+"""
 function sample_stabilizer(x::Vector{Int})
     n = length(x)
     for d in sort(divisors(n))
@@ -27,8 +37,22 @@ function sample_stabilizer(x::Vector{Int})
     return 0
 end
 
+""" 
+    group_action(x::Vector{Int}, j::Int)::Vector{Int}
+
+Cyclically shift x by j.
+"""
 group_action(x::Vector{Int}, j::Int) = [x[(j+1):end]; x[1:j]]
 
+"""
+    burnside_proccess(n::Int, reps::Int, k=2::Int)
+
+Run the Burnside process for the cyclic group acting on k-ary strings.
+
+Return
+    xs: List of vectors [x_1, x_2,..., x_reps].
+    js: Group elements [j_1, j_2,...,j_reps].
+"""
 function burnside_proccess(n::Int, reps::Int, k=2::Int)
     x = zeros(Int, n)
     xs = [x]
